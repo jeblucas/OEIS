cc78e1ca728970e72e2ca160572e036ee76bd38e
Jeb Adams <jeba@amgen.com>
2020-05-07 09:50:53 -0700

    first commit, included R script for https://oeis.org/A248651

---
+++ MySeq.R
@@ -0,0 +1,51 @@
+digits <- c(0:9) #vector of the digits
+digitCounts <- rep(0L,10) #vector for digit count tracking
+A <- c() #vector of entries
+
+
+A[1] <- 1L #first entry
+failed <- 1L #failed entry
+digitCounts[2] = 1L #impact of first entry on digitCounts
+
+#Build a function that turns a number into a vector of its component digits
+digits <- function(x) {
+  if(length(x) > 1 ) {
+    lapply(x, digits)
+  } else {
+    n <- floor(log10(x))+1L
+    rev( x %/% 10^seq(0, length.out=n) %% 10 )
+  }
+}
+
+while (length(A) < 101) {
+  i <- max(failed, max(A)) + 1
+  newCounts <- digitCounts
+  digitList <- digits(i)
+  
+  for (j in digitList) {
+    newCounts[j + 1] <- newCounts[j + 1] + 1
+  }
+  
+  if (max(newCounts) - min(newCounts) > 1) {
+       failed <- i } else {
+        digitCounts <- newCounts
+        A <- c(A,as.integer(i))
+      }
+    
+}
+# 
+# for (i > max(A)){
+#   newCounts <- digitCounts
+#   digitList <- digits(i)
+#   for {j in digitList}(
+#     newCounts[j + 1] + 1
+#     if max(newCounts) - min(newCounts) > 1 {
+#       exit } else {
+#         digitCounts <-newCounts
+#         A <- c(A,i)
+#       }
+#     
+#   )
+#   
+# }
+# 
